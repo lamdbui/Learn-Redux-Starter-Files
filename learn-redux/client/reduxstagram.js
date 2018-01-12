edea4d9b0677f9f93702c6efb16beec4be1a54c9
Lam Bui <lamdbui@gmail.com>
2018-01-12 15:11:07 -0800

    Add Component files and setup initial router template

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -3,6 +3,9 @@ import React from 'react';
 import { render } from 'react-dom';
 
 // Import CSS
-import css from './styles/style.styl';
+import './styles/style.styl';
 
-render(<p>hi</p>, document.getElementById('root'));
+// Import Components
+import Main from './components/Main';
+
+render(<Main />, document.getElementById('root'));
