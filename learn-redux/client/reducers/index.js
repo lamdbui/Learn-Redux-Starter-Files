52a6606077548ecb3c9d5882231f6a6873c7e917
Lam Bui <lamdbui@gmail.com>
2018-01-13 15:50:39 -0800

    Add notes about action and reducer behavior

--- learn-redux/client/reducers/index.js
+++ learn-redux/client/reducers/index.js
@@ -4,6 +4,9 @@ import { routerReducer } from 'react-router-redux';
 import posts from './posts';
 import comments from './comments';
 
+// NOTE: for every action, it gets dispatched to all reducers available. It
+// is up the each reducer to decide if it is important and do something
+
 const rootReducer = combineReducers({posts, comments, routing: routerReducer});
 
 export default rootReducer;
