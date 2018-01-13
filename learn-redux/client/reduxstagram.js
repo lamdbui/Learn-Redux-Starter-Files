5518d0eeccae02853933ec589ce24ea968d52de3
Lam Bui <lamdbui@gmail.com>
2018-01-13 13:30:09 -0800

    Create Redux store for use

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -7,5 +7,19 @@ import './styles/style.styl';
 
 // Import Components
 import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
+
+// Import react router deps
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+    <IndexRoute component={PhotoGrid}></IndexRoute>
+    <Route path="/view/:postId"></Route>
+    </Route>
+  </Router>
+);
 
 render(<Main />, document.getElementById('root'));
