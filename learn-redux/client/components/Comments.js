fa6bc9f955d3258331adcde3d1f82eff90985298
Lam Bui <lamdbui@gmail.com>
2018-01-15 14:23:34 -0800

    Hook up submit button and inputs to handler

--- learn-redux/client/components/Comments.js
+++ learn-redux/client/components/Comments.js
@@ -13,11 +13,22 @@ const Comments = React.createClass({
     )
   },
 
+  handleSubmit(e) {
+    // stop the page from refreshing
+    e.preventDefault();
+    console.log('Submitting the form');
+    console.log(this.refs);
+    const { postId } = this.props.params;
+    const author = this.refs.author.value;
+    const comment = this.refs.comment.value;
+    console.log(postId, author, comment);
+  },
+
   render() {
     return (
       <div className="comments">
         {this.props.postComments.map(this.renderComment)}
-        <form ref="commentForm" className="comment-form">
+        <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit}>
           <input type="text" ref="author" placeholder="author" />
           <input type="text" ref="comment" placeholder="comment" />
           <input type="submit" hidden />
