bd7c2099e4fb6d5074885c033d3c60a4b26cc87e
Lam Bui <lamdbui@gmail.com>
2018-01-15 10:24:30 -0800

    Hook like button into redux and fix typo with CSSTransitionGroup

--- learn-redux/client/components/Photo.js
+++ learn-redux/client/components/Photo.js
@@ -15,7 +15,7 @@ const Photo = React.createClass({
               className="grid-photo" />
           </Link>
 
-          <CSSTransitionGroup transition="like"
+          <CSSTransitionGroup transitionName="like"
             transitionEnterTimeout={500}
             transtionLeaveTimeout={500}>
             <span key={post.likes} className="likes-heart">{post.likes}</span>
@@ -26,7 +26,7 @@ const Photo = React.createClass({
         <figcaption>
           <p>{post.caption}</p>
           <div className="control-buttons">
-            <button className="likes">&hearts; {post.likes}</button>
+            <button onClick={this.props.increment.bind(null, i)} className="likes">&hearts; {post.likes}</button>
             <Link className="button" to={`/view/${post.code}`}>
               <span className="comment-count">
                 <span className="speech-bubble"></span>
--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -4,9 +4,19 @@
 // 2. copy of the current state
 
 function posts(state = [], action) {
-  console.log('The post will change');
-  console.log(state, action);
-  return state;
+  switch(action.type) {
+    case 'INCREMENT_LIKES':
+      // return the updated state
+      console.log('Incrementing Likes');
+      const i = action.index;
+      return [
+        ...state.slice(0, i),   // before the one we are updating
+        {...state[i], likes: state[i].likes + 1}, // update the one we want
+        ...state.slice(i + 1),  // after the one we are updating
+      ];
+    default:
+      return state;
+  }
 }
 
 export default posts;
